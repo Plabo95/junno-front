f51a8d64df4e8fe2aea86704265bfb22a0260b68
Plabo95 <pablo.novo@ara-tech.es>
2022-08-24 11:55:04 +0200

    sidebar hover and routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Register from "./pages/Register";
 import Home from './pages/Home';
 import Searcher from './pages/Searcher';
 import Files from './pages/Files';
+import List from './pages/List'
+import Visor from './pages/Visor'
+import Docs from './pages/Docs'
 
 export default function App() {
   return (
@@ -16,8 +19,14 @@ export default function App() {
           <Route index path='/' element={<Home/>} />  
           <Route path='/register' element={<Register/>} /> 
           <Route path='/login' element={<Login/>} />
+
+          {/* Dentro APP */}
           <Route path='/buscador' element={<Searcher/>} />
           <Route path='/archivos' element={<Files/>} />
+          <Route path='/listado' element={<List/>} />
+          <Route path='/visor' element={<Visor/>} />
+          <Route path='/documentos' element={<Docs/>} />
+
           <Route path="*" element={<p>There's nothing here: 404!</p>} />
       </Routes>
     </>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Flex, Link } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Sidebar(){
 
     let navigate = useNavigate();
+    const location = useLocation();
 
     return(
 
         <Flex direction='column' bg='#005E2A' minW='15%' align='center'  gap='1em' minH='90vh' py='2em' px='2em'>
-            <Link align='center' fontSize='20px' color='white' onClick={()=>navigate('/buscador')} > Buscador de inmuebles</Link>
-            <Link align='center' fontSize='20px' color='white' onClick={()=>navigate('/archivos')}> Archivos </Link>
-            <Link align='center' fontSize='20px' color='white' > Listado de fincas </Link>
-            <Link align='center' fontSize='20px' color='white' > Visor ambiental </Link>
-            <Link align='center' fontSize='20px' color='white' > Documentos </Link>
+            <Link align='center' fontSize='20px'  color='white' onClick={()=>navigate('/buscador')} textDecor={location.pathname ==='/buscador'? 'underline' : 'none' }> 
+                Buscador de inmuebles</Link>
+            <Link align='center' fontSize='20px' color='white' onClick={()=>navigate('/archivos')} textDecor={location.pathname ==='/archivos'? 'underline' : 'none' }>
+                Archivos </Link>
+            <Link align='center' fontSize='20px' color='white' onClick={()=>navigate('/listado')} textDecor={location.pathname ==='/listado'? 'underline' : 'none' }> 
+                Listado de fincas </Link>
+            <Link align='center' fontSize='20px' color='white' onClick={()=>navigate('/visor')} textDecor={location.pathname ==='/visor'? 'underline' : 'none' }>
+                Visor ambiental </Link>
+            <Link align='center' fontSize='20px' color='white' onClick={()=>navigate('/documentos')} textDecor={location.pathname ==='/documentos'? 'underline' : 'none' }
+                >Documentos </Link>
         </Flex>
 
     )
