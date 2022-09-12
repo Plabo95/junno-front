1316b87ecc946ff887cd9ad767d61ec66678f9bb
Plabo95 <plabonovo@gmail.com>
2022-09-12 12:09:08 +0200

    solicitud PIN

--- src/components/Docs/DocsPIN.jsx
+++ src/components/Docs/DocsPIN.jsx
@@ -1,24 +1,28 @@
 import { Flex, Text, Button, Input } from "@chakra-ui/react"
+import { useDisclosure } from "@chakra-ui/react"
 
+//components 
+import PinModal from "./PinModal"
 
 export default function DocsPIN({setState}){
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return(
 
         <Flex w='100%'  pt='2em' direction='column' gap='2em'>
             <Text  px='5%' fontWeight='500' fontSize='32px' color='green'>Solicitud de certificado o informe urbanístico</Text>
             <Text px='5%' fontWeight='500' fontSize='16px'>
-                Para comenzar puedes elegir el método de autenticación con el que deseas solicitar el certificado :
+                Completa los datos:
             </Text>
-            <Flex direction='column' gap='1em' >
+            <Flex direction='column' gap='1em' px='5%' maxW='40%'>
                 <Text color='green' fontWeight='600' >Clave PIN</Text>
                 <Text color='green' fontWeight='600' >Introduzca su DNI/NIE</Text>
                 <Input/>
                 <Text color='green' fontWeight='600' >Introduzca la fecha de validez de su DNI (o fecha de su expedición si es un DNI permanente)</Text>
                 <Input/>
-                <Button colorScheme='green' >Validar</Button>
+                <Button mt='2em' w='50%' colorScheme='green' onClick={onOpen} >Validar</Button>
             </Flex>
+            <PinModal isOpen={isOpen} onClose={onClose} setState={setState}/>
         </Flex>
-
     )
 }
---
+++ src/components/Docs/PinModal.jsx
@@ -0,0 +1,20 @@
+import {Modal, ModalOverlay, ModalContent, Input, Text, Button, Flex } from "@chakra-ui/react"
+
+import success from '../../img/documentos/success.png'
+
+export default function PinModal({isOpen, onClose, setState}){
+
+    return(
+        <Modal  isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent p='2em'  justify='center' gap='1em'>
+                <Text fontWeight='600' color='green' fontSize='24px' > Introduzca la clave </Text>
+                <Text fontWeight='400' fontSize='20px' >Clave</Text>
+                <Flex gap='2em'>
+                    <Input bg='#F5F5F5' w='40%' />  
+                    <Button colorScheme='green' onClick={ ()=> setState('manual2') } >Enviar</Button>
+                </Flex>      
+            </ModalContent>
+        </Modal>
+    )
+}
